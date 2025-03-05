4e4a98b3e6619a1a4e9412c78d570bc7bef467ba
Mostafa-3ta <m2ostafa2000@gmail.com>
2025-03-05 19:25:58 +0200

    edit

--- app/components/Seasons.tsx
+++ app/components/Seasons.tsx
@@ -10,16 +10,14 @@ export default function Seasons({ ShowDetails, ShowId, withHr = true }: { ShowDe
     return <>
 
         <Heading center title="Seasons" />
-        <SwiperCards spaceBetween={15} pauseOnMouseEnter slidesPerView={4} className='text-center items-center h-full px-2 bg-black/30 rounded-lg'
+        <SwiperCards spaceBetween={20} xsSlides={{ slidesPerView: 2, spaceBetween: 20 }} smSlides={{ slidesPerView: 2, spaceBetween: 20 }} mdSlides={{ slidesPerView: 3, spaceBetween: 20 }} lgSlides={{ slidesPerView: 4, spaceBetween: 20 }} pauseOnMouseEnter className='text-center items-center h-full px-2 bg-black/30 rounded-lg'
             items={ShowDetails?.seasons?.map((show: Season) => ({
-                card: <div className='group text-center min-h-full my-8' key={show.id} >
-                    <Link href={`Season/?id=${ShowId}&season=${show.season_number}`}>
-                        <div className="w-full h-96 rounded-md overflow-hidden relative">
-                            <div className='flex w-full relative flex-col items-center'>
-                                <CustomImg className='w-full h-96 object-contain duration-200 group-hover:scale-110 z-10 rounded-md mb-4' imgSrc={show.poster_path} />
-                            </div>
+                card: <div className='group text-center min-h-full my-4' key={show.id} >
+                    <Link href={`Season/?id=${ShowId}&season=${show.season_number}`} className='w-full h-full'>
+                        <div className='flex relative overflow-hidden flex-col items-center'>
+                            <CustomImg className='w-60 h-full object-contain duration-200 group-hover:scale-110 z-10 rounded-md mb-4' imgSrc={show.poster_path} />
+                            <h4 className='duration-200 group-hover:text-fuchsia-400 mt-2 font-semibold'> {show.name}</h4>
                         </div>
-                        <h4 className='duration-200 group-hover:text-fuchsia-400 mt-2 font-semibold'> {show.name}</h4>
                     </Link>
                 </div >
             })
--- app/components/Similar.tsx
+++ app/components/Similar.tsx
@@ -8,7 +8,7 @@ export default function Similar({ Recommend, detailsLink, showType = false, hide
     return (
         <div className="my-3 md:mx-6 mx-2 customScrollBar">
             {!hideTitle && <Heading center title={`Similar ${showType ? 'Shows' : 'Movies'}`} />}
-            <SwiperCards spaceBetween={20} xsSlides={{ slidesPerView: 2, spaceBetween: 20 }} smSlides={{ slidesPerView: 2, spaceBetween: 20 }} mdSlides={{ slidesPerView: 3, spaceBetween: 20 }} lgSlides={{ slidesPerView: 4, spaceBetween: 20 }} pauseOnMouseEnter slidesPerView={4} className='text-center items-center h-full bg-black/30 rounded-lg'
+            <SwiperCards spaceBetween={20} xsSlides={{ slidesPerView: 2, spaceBetween: 20 }} smSlides={{ slidesPerView: 2, spaceBetween: 20 }} mdSlides={{ slidesPerView: 3, spaceBetween: 20 }} lgSlides={{ slidesPerView: 4, spaceBetween: 20 }} pauseOnMouseEnter className='text-center items-center h-full bg-black/30 rounded-lg'
                 items={Recommend?.results?.map((show: any) => ({
                     card: <div className='group min-h-full my-8 ' key={show.id} >
                         <MovieCard show={showType} movie={show} pageLink={detailsLink} />
